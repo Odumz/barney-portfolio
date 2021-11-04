511846c33c648cf5f982c5167df8a949c4d340a2
Odumz <odumz20@gmail.com>
2021-11-04 01:23:22 +0100

    landing components started

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,12 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
+  tailwindcss: {
+    cssPath: '~/assets/main.css',
+    configPath: 'tailwind.config.js',
+    exposeConfig: false,
+    config: {}
+  },
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <Tutorial/>
+  <div class="bg-gray-100">
+    <Topbar class="pt-14" />
+    <Intro class="my-7" />
+    <!-- <Tutorial/> -->
+    <CaseStudyList class="bg-white" />
+    <Outreach class="bg-white" />
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style>
+  fontface {
+    font-family: 'Rubik', sans-serif;
+  }
+
+</style>
