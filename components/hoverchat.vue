ceae8fba25b14a1eafdab74226ab38bab88a078e
Odumz <odumz20@gmail.com>
2021-11-24 20:16:39 +0100

    float mailto div added; responsiveness fixes added

---
+++ components/hoverchat.vue
@@ -0,0 +1,12 @@
+<template>
+  <a id="email" href="mailto:barnabasinyangsam@yahoo.com" class="h-24 w-24 text-skin-primary py-10 px-4 flex flex-col rounded justify-center items-center shadow-lg">
+    <img src="https://res.cloudinary.com/griffintech/image/upload/v1637770790/barney/message-icon_hp5qha.png" alt="" class="transform scale-50">
+    <p class="uppercase text-xs text-center pad">Email me</p>
+  </a>
+</template>
+
+<style>
+.pad {
+
+}
+</style>
